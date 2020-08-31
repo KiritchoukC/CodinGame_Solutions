75b9dbda68fafc67255328a37005eb8b4544ad16
KiritchoukC <clement.kiritchouk@gmail.com>
2020-08-31 20:02:34 +0200

    update rock-paper-scissors-lizard-spock solution

--- rock-paper-scissors-lizard-spock/Main.fsx
+++ rock-paper-scissors-lizard-spock/Main.fsx
@@ -18,41 +18,34 @@ let players =
           Symbol = token.[1]
           Opponents = List.empty })
 
-let getTieBattleWinner player1 player2 =
-    if player1.Number < player2.Number then player1 else player2
-
 let battle player1 player2 =
     let winner1 = { player1 with Opponents = player1.Opponents @ [player2.Number] }
     let winner2 = { player2 with Opponents = player2.Opponents @ [player1.Number] }
     match winner1.Symbol, winner2.Symbol with
     | "C", "P" -> winner1
-    | "P", "C" -> winner2
-    | "P", "R" -> winner1
-    | "R", "P" -> winner2
-    | "R", "L" -> winner1
-    | "L", "R" -> winner2
-    | "L", "S" -> winner1
-    | "S", "L" -> winner2
-    | "S", "C" -> winner1
-    | "C", "S" -> winner2
-    | "C", "L" -> winner1
-    | "L", "C" -> winner2
-    | "L", "P" -> winner1
-    | "P", "L" -> winner2
-    | "P", "S" -> winner1
-    | "S", "P" -> winner2
-    | "S", "R" -> winner1
-    | "R", "S" -> winner2
     | "R", "C" -> winner1
+    | "P", "R" -> winner1
+    | "R", "L" -> winner1
+    | "L", "S" -> winner1
+    | "S", "C" -> winner1
+    | "C", "L" -> winner1
+    | "L", "P" -> winner1
+    | "P", "S" -> winner1
+    | "S", "R" -> winner1
+    
+    | "P", "C" -> winner2
+    | "R", "P" -> winner2
+    | "L", "R" -> winner2
+    | "S", "L" -> winner2
+    | "C", "S" -> winner2
+    | "L", "C" -> winner2
+    | "P", "L" -> winner2
+    | "S", "P" -> winner2
+    | "R", "S" -> winner2
     | "C", "R" -> winner2
-    | s1, s2 when s1 = s2 ->
-        getTieBattleWinner winner1 winner2
-    | _ ->
-        Console.Out.WriteLine "Battle pattern not found"
-        winner1
-
-
-let splitList list = List.foldBack (fun x (l, r) -> x :: r, l) list ([], [])
+    
+    | _ -> match winner1.Number < winner2.Number with
+           | true -> winner1 | false -> winner2
 
 let rec runPool players =
     let playersLeft =
