7d0b23f6f40b7b3320dd1fc7499cde48ed280ea6
KiritchoukC <clement.kiritchouk@gmail.com>
2020-09-03 21:46:49 +0200

    init rectangle-partition

---
+++ rectangle-partition/main.fsx
@@ -0,0 +1,22 @@
+﻿module String =
+    let split separator (str: string) = str.Split separator
+
+open System
+
+let read () = Console.In.ReadLine()
+
+let extractDataFromString = String.split [| ' ' |] >> Array.map int
+
+let (w, h, countX, countY) =
+//    read ()
+    "10 5 2 1"
+    |> extractDataFromString
+    |> (fun x -> (x.[0], x.[1], x.[2], x.[3]))
+
+//let xs = read () |> extractDataFromString
+let xs = "2 5" |> extractDataFromString
+
+//let ys = read () |> extractDataFromString
+let ys = "3" |> extractDataFromString
+
+
