1b88c862cdbe6116bd61c7e210e89a6b2e600a4a
KiritchoukC <clement.kiritchouk@gmail.com>
2020-09-04 21:24:25 +0200

    add rectangle-partition solution

--- rectangle-partition/main.fsx
+++ rectangle-partition/main.fsx
@@ -3,20 +3,50 @@
 
 open System
 
+
+let filterSome lst =
+    lst
+    |> List.filter Option.isSome
+    |> List.map Option.get
+
 let read () = Console.In.ReadLine()
 
-let extractDataFromString = String.split [| ' ' |] >> Array.map int
+let extractDataFromString = String.split [| ' ' |] >> Array.map int >> Array.toList
 
 let (w, h, countX, countY) =
-//    read ()
-    "10 5 2 1"
+    read ()
     |> extractDataFromString
     |> (fun x -> (x.[0], x.[1], x.[2], x.[3]))
 
-//let xs = read () |> extractDataFromString
-let xs = "2 5" |> extractDataFromString
+let xs = read () |> extractDataFromString
 
-//let ys = read () |> extractDataFromString
-let ys = "3" |> extractDataFromString
+let ys = read () |> extractDataFromString
+    
+let getAllLengths ns n = 
+    ns
+    |> List.filter (fun n' -> n' < n)
+    |> List.map (fun n' -> n - n')
 
+let allXs = xs |> (@) [w]
 
+let allYs = ys |> (@) [h]
+
+let xLengths =
+    allXs
+    |> List.map (getAllLengths xs)
+    |> List.concat
+    |> (@) allXs
+    
+let yLengths =
+    allYs
+    |> List.map (getAllLengths ys)
+    |> List.concat
+    |> (@) allYs
+    
+xLengths
+|> List.map (fun x ->
+    yLengths
+    |> List.filter (fun y -> y = x))
+|> List.concat
+|> List.length
+|> printfn "%i"
