bbff1ea3f6cca907d8abf7e1fccb7b29317366a9
KiritchoukC <clement.kiritchouk@gmail.com>
2020-09-14 20:57:55 +0200

    solve pikaptcha ep02

--- Pikaptcha/ep02/main.fsx
+++ Pikaptcha/ep02/main.fsx
@@ -13,23 +13,18 @@ type Side =
     | Left
     | Right
 
-//let (width, height) =
-//    (Console.In.ReadLine()).Split [| ' ' |]
-//    |> Array.map int
-//    |> fun c -> (c.[0], c.[1])
-//
-//let lines = [ 0 .. height - 1 ] |> List.map (fun c -> Console.In.ReadLine())
-//
-//let side =
-//    match Console.In.ReadLine() with
-//    | "R" -> Right
-//    | "L" -> Left
-//    | _ -> failwith "Wrong side input"
+let (width, height) =
+    (Console.In.ReadLine()).Split [| ' ' |]
+    |> Array.map int
+    |> fun c -> (c.[0], c.[1])
 
-let (width, height) = (5, 3)
-let lines = [ ">000#"; "#0#00"; "00#0#" ]
+let lines = [ 0 .. height - 1 ] |> List.map (fun c -> Console.In.ReadLine())
 
-let side = Left
+let side =
+    match Console.In.ReadLine() with
+    | "R" -> Right
+    | "L" -> Left
+    | _ -> failwith "Wrong side input"
 
 let maze0 = Array2D.init height width (fun y x -> lines.[y].[x])
 
@@ -39,47 +34,60 @@ let (x0, y0, orientation0) =
         let source = Array2D.find o maze0
         match source with
         | None -> None
-        | Some coords -> Some(fst coords, snd coords, o))
+        | Some coords -> Some(snd coords, fst coords, o))
     |> List.filter Option.isSome
     |> List.map Option.get
     |> List.head
 
+let getCell x y maze = 
+    try
+        Some (Array2D.get maze y x)
+    with
+    |  :? IndexOutOfRangeException -> None
+
 let incrementCell x y maze =
     let mazeCopy = Array2D.copy maze
-    let current = Array2D.get mazeCopy y x
+    let current = getCell x y mazeCopy
 
     match current with
-    | '#' -> mazeCopy
-    | '>' | '<' | '^' | 'v' ->
-        Array2D.set mazeCopy y x '1'
-        mazeCopy
-    | c ->
-        let incremented =
-            c
-            |> string
-            |> int
-            |> (+) 1
-            |> string
-            |> Seq.head
-        Array2D.set mazeCopy y x incremented
-        mazeCopy
+    | None -> mazeCopy
+    | Some c ->
+        match c with
+        | '#' -> mazeCopy
+        | '>' | '<' | '^' | 'v' ->
+            Array2D.set mazeCopy y x '1'
+            mazeCopy
+        | c ->
+            let incremented =
+                c
+                |> string
+                |> int
+                |> (+) 1
+                |> string
+                |> Seq.head
+            Array2D.set mazeCopy y x incremented
+            mazeCopy
+
 
 let go x y maze =
-    let isOut = x = width || y = height || x < 0 || y < 0
+    let isOut = x >= width || y >= height || x < 0 || y < 0
     match isOut with
     | true -> None
-    | _ ->
-        let nextCell = Array2D.get maze y x
-        let isDone =
-            not (nextCell = '0') && (x = x0 && y = y0)
-        match isDone with
-        | true -> Some(0, 0, maze, true)
-        | false ->
-            match nextCell with
-            | '#' -> None
-            | c ->
-                let updatedMaze = incrementCell x y maze
-                Some(x, y, updatedMaze, false)
+    | false ->
+        let nextCell = getCell x y maze
+        match nextCell with
+        | None -> None
+        | Some cell ->
+            let isDone =
+                not (cell = '0') && (x = x0 && y = y0)
+            match isDone with
+            | true -> Some(0, 0, maze, true)
+            | false ->
+                match cell with
+                | '#' -> None
+                | _ ->
+                    let updatedMaze = incrementCell x y maze
+                    Some(x, y, updatedMaze, false)
 
 let goRight x y = go (x + 1) y
 let goLeft x y = go (x - 1) y
@@ -87,24 +95,45 @@ let goDown x y = go x (y + 1)
 let goUp x y = go x (y - 1)
 
 let rec goOn orientation x y maze =
-    match orientation with
-    | '>' ->
-        match goUp x y maze with
-        | Some (tx, ty, tMaze, isDone) -> (tx, ty, '^', tMaze, isDone)
-        | None -> goOn 'v' x y maze
-    | 'v' ->
-        match goRight x y maze with
-        | Some (tx, ty, tMaze, isDone) -> (tx, ty, '>', tMaze, isDone)
-        | None -> goOn '<' x y maze
-    | '<' ->
-        match goDown x y maze with
-        | Some (tx, ty, tMaze, isDone) -> (tx, ty, 'v', tMaze, isDone)
-        | None -> goOn '^' x y maze
-    | '^' ->
-        match goLeft x y maze with
-        | Some (tx, ty, tMaze, isDone) -> (tx, ty, '<', tMaze, isDone)
-        | None -> goOn '>' x y maze
-    | _ -> failwith "wrong orientation"
+    match side with
+    | Left -> 
+        match orientation with
+        | '>' ->
+            match goUp x y maze with
+            | Some (tx, ty, tMaze, isDone) -> (tx, ty, '^', tMaze, isDone)
+            | None -> goOn 'v' x y maze
+        | 'v' ->
+            match goRight x y maze with
+            | Some (tx, ty, tMaze, isDone) -> (tx, ty, '>', tMaze, isDone)
+            | None -> goOn '<' x y maze
+        | '<' ->
+            match goDown x y maze with
+            | Some (tx, ty, tMaze, isDone) -> (tx, ty, 'v', tMaze, isDone)
+            | None -> goOn '^' x y maze
+        | '^' ->
+            match goLeft x y maze with
+            | Some (tx, ty, tMaze, isDone) -> (tx, ty, '<', tMaze, isDone)
+            | None -> goOn '>' x y maze
+        | _ -> failwith "wrong orientation"
+    | Right -> 
+        match orientation with
+        | '<' ->
+            match goUp x y maze with
+            | Some (tx, ty, tMaze, isDone) -> (tx, ty, '^', tMaze, isDone)
+            | None -> goOn 'v' x y maze
+        | '^' ->
+            match goRight x y maze with
+            | Some (tx, ty, tMaze, isDone) -> (tx, ty, '>', tMaze, isDone)
+            | None -> goOn '<' x y maze
+        | '>' ->
+            match goDown x y maze with
+            | Some (tx, ty, tMaze, isDone) -> (tx, ty, 'v', tMaze, isDone)
+            | None -> goOn '^' x y maze
+        | 'v' ->
+            match goLeft x y maze with
+            | Some (tx, ty, tMaze, isDone) -> (tx, ty, '<', tMaze, isDone)
+            | None -> goOn '>' x y maze
+        | _ -> failwith "wrong orientation"
 
 let printAnswer maze =
     maze
@@ -121,4 +150,24 @@ let rec run orientation x y maze =
 
 let maze1 = incrementCell x0 y0 maze0
 
-run orientation0 x0 y0 maze1
+let getFreeCell x y maze =
+    match getCell x y maze with
+    | None -> None
+    | Some '#' -> None
+    | Some freeCell -> Some freeCell 
+    
+let isTrapped x y maze =
+    let upBloc = getFreeCell x (y + 1) maze
+    let downBloc = getFreeCell x (y - 1) maze
+    let leftBloc = getFreeCell (x - 1) y maze
+    let rightBloc = getFreeCell (x + 1) y maze
+    match (upBloc, downBloc, leftBloc, rightBloc) with
+    | (None, None, None, None) -> true
+    | _ -> false
+
+match isTrapped x0 y0 maze1 with
+| true ->
+    let trappedMaze = Array2D.copy maze0
+    Array2D.set trappedMaze y0 x0 '0'
+    printAnswer trappedMaze
+| false -> run orientation0 x0 y0 maze1
