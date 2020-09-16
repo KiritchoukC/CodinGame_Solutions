fdd8a074828ae8d8a5d3bbe24f389dee1f4eaa13
KiritchoukC <clement.kiritchouk@gmail.com>
2020-09-16 13:52:15 +0200

    refactor batman ep01

--- Batman/Ep01/fsharp/main.fsx
+++ Batman/Ep01/fsharp/main.fsx
@@ -15,32 +15,42 @@ type Limit =
 let token = (Console.In.ReadLine()).Split [| ' ' |]
 let W = int (token.[0])
 let H = int (token.[1])
-let N = int (Console.In.ReadLine()) 
+let N = int (Console.In.ReadLine())
 let token1 = (Console.In.ReadLine()).Split [| ' ' |]
 let X0 = int (token1.[0])
 let Y0 = int (token1.[1])
 
-eprintfn "%A" (W, H)
-
-let goUp (x: int) (y: int) limit =
+let goUp ((x: int), (y: int), limit) =
     let nextY = int (Math.Floor(float (y - limit.Vertical.Low) / 2.)) + limit.Vertical.Low
-    let newLimit = {limit with Vertical = {limit.Vertical with High = y}}
+    let newLimit = { limit with Vertical = { limit.Vertical with High = y } }
     (x, nextY, newLimit)
-    
-let goRight (x: int) (y: int) limit =
+
+let goRight ((x: int), (y: int), limit) =
     let nextX = int (Math.Floor(float (limit.Horizontal.High - x) / 2.)) + x
-    let newLimit = {limit with Horizontal = {limit.Horizontal with Low = x}}
+    let newLimit = { limit with Horizontal = { limit.Horizontal with Low = x } }
     (nextX, y, newLimit)
-    
-let goDown (x: int) (y: int) limit =
+
+let goDown ((x: int), (y: int), limit) =
     let nextY = int (Math.Floor(float (limit.Vertical.High - y) / 2.)) + y
-    let newLimit = {limit with Vertical = {limit.Vertical with Low = y}}
+    let newLimit = { limit with Vertical = { limit.Vertical with Low = y } }
     (x, nextY, newLimit)
-    
-let goLeft (x: int) (y: int) limit =
+
+let goLeft ((x: int), (y: int), limit) =
     let nextX = int (Math.Floor(float (x - limit.Horizontal.Low) / 2.)) + limit.Horizontal.Low
-    let newLimit = {limit with Horizontal = {limit.Horizontal with High = x}}
+    let newLimit = { limit with Horizontal = { limit.Horizontal with High = x } }
     (nextX, y, newLimit)
+
+let move bombDir =
+    match bombDir with
+    | "U" -> goUp
+    | "UR" -> goUp >> goRight
+    | "R" -> goRight
+    | "DR" -> goDown >> goRight
+    | "D" -> goDown
+    | "DL" -> goDown >> goLeft
+    | "L" -> goLeft
+    | "UL" -> goUp >> goLeft
+    | _ -> failwith "wrong input"
 
 let rec findNextWindow x y limit =
     let bombDir = Console.In.ReadLine()
@@ -48,17 +58,7 @@ let rec findNextWindow x y limit =
     match bombDir with
     | "" -> ()
     | _ ->
-        let (nextX, nextY, newLimit) =
-            match bombDir with
-            | "U" -> goUp x y limit
-            | "UR" -> goUp x y limit |||> goRight
-            | "R" -> goRight x y limit
-            | "DR" -> goDown x y limit |||> goRight
-            | "D" -> goDown x y limit
-            | "DL" -> goDown x y limit |||> goLeft
-            | "L" -> goLeft x y limit
-            | "UL" -> goUp x y limit |||> goLeft
-            | _ -> failwith "wrong input"
+        let (nextX, nextY, newLimit) = move bombDir (x, y, limit)
 
         printfn "%i %i" (nextX) (nextY)
         findNextWindow nextX nextY newLimit
