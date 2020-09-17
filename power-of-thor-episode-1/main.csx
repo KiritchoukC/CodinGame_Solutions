5f43ef66be1436d00273804523f654a48d7dfb2b
KiritchoukC <clement.kiritchouk@gmail.com>
2020-09-17 13:54:49 +0200

    add c# and f# solution for power-of-thor-episode-1

---
+++ power-of-thor-episode-1/main.csx
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ * ---
+ * Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders.
+ **/
+class Player
+{
+    static void Main(string[] args)
+    {
+        string[] inputs = Console.ReadLine().Split(' ');
+        int lightX = int.Parse(inputs[0]); // the X position of the light of power
+        int lightY = int.Parse(inputs[1]); // the Y position of the light of power
+        int initialTX = int.Parse(inputs[2]); // Thor's starting X position
+        int initialTY = int.Parse(inputs[3]); // Thor's starting Y position
+        int remainingXMoves = lightX - initialTX;
+        int remainingYMoves = lightY - initialTY;
+
+        // game loop
+        while (true)
+        {
+            int remainingTurns = int.Parse(Console.ReadLine()); // The remaining amount of turns Thor can move. Do not remove this line.
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+            
+            string direction = "";
+            if(remainingXMoves == 0 || remainingYMoves == 0){
+                MoveStraight(remainingXMoves, "X");
+                MoveStraight(remainingYMoves, "Y");
+            }else{
+                if(remainingXMoves > 0 && remainingYMoves > 0){ 
+                    direction = "SE";
+                    remainingYMoves--;
+                    remainingXMoves--;
+                }
+                if(remainingXMoves < 0 && remainingYMoves > 0){ 
+                    direction = "SW";
+                    remainingYMoves--;
+                    remainingXMoves++;
+                }
+                if(remainingXMoves > 0 && remainingYMoves < 0){ 
+                    direction = "NE";
+                    remainingYMoves++;
+                    remainingXMoves--;
+                }
+                if(remainingXMoves < 0 && remainingYMoves < 0){ 
+                    direction = "NW";
+                    remainingYMoves++;
+                    remainingXMoves++;
+                }
+            }
+            
+            // A single line providing the move to be made: N NE E SE S SW W or NW
+            Console.WriteLine(direction);
+        }
+    }
+    
+    private static void MoveStraight(int remainingMoves, string axis){
+        string direction = "";
+        while(remainingMoves !=0){
+            if(remainingMoves > 0){
+                direction = axis == "X" ? "E" : "S";
+                remainingMoves--;
+            }
+            if(remainingMoves < 0){
+                direction = axis == "X" ? "W" : "N";
+                remainingMoves++;
+            }
+            Console.WriteLine(direction);
+        }
+    }
+}
---
+++ power-of-thor-episode-1/main.fsx
@@ -0,0 +1,104 @@
+﻿open System
+
+module Domain =
+    type Coords =
+        { X: int
+          Y: int }
+
+    type State =
+        { Light: Coords
+          Thor: Coords }
+
+        static member Create lightX lightY thorX thorY =
+            { Light =
+                  { X = lightX
+                    Y = lightY }
+              Thor =
+                  { X = thorX
+                    Y = thorY } }
+
+        member this.Move dir =
+            match dir with
+            | "N" ->
+                { this with
+                      Thor =
+                          { X = this.Thor.X
+                            Y = this.Thor.Y - 1 } }
+            | "NE" ->
+                { this with
+                      Thor =
+                          { X = this.Thor.X + 1
+                            Y = this.Thor.Y - 1 } }
+            | "E" ->
+                { this with
+                      Thor =
+                          { X = this.Thor.X + 1
+                            Y = this.Thor.Y } }
+            | "SE" ->
+                { this with
+                      Thor =
+                          { X = this.Thor.X + 1
+                            Y = this.Thor.Y + 1 } }
+            | "S" ->
+                { this with
+                      Thor =
+                          { X = this.Thor.X
+                            Y = this.Thor.Y + 1 } }
+            | "SW" ->
+                { this with
+                      Thor =
+                          { X = this.Thor.X - 1
+                            Y = this.Thor.Y + 1 } }
+            | "W" ->
+                { this with
+                      Thor =
+                          { X = this.Thor.X - 1
+                            Y = this.Thor.Y } }
+            | "NW" ->
+                { this with
+                      Thor =
+                          { X = this.Thor.X - 1
+                            Y = this.Thor.Y - 1 } }
+            | x -> failwithf "Wrong input: %s" x
+
+module Data =
+
+    let (lightX, lightY, initialTX, initialTY) =
+        let inputs = (Console.In.ReadLine()).Split [| ' ' |] |> Array.map int
+        (inputs.[0], inputs.[1], inputs.[2], inputs.[3])
+
+module Application =
+    open Domain
+    open Data
+
+    let toSign axis =
+        match axis with
+        | x when x = 0 -> '0'
+        | x when x > 0 -> '+'
+        | x when x < 0 -> '-'
+        | _ -> failwith "unexpected axis"
+
+    let rec run (state: State): unit =
+        let remainingTurns =
+            int (Console.In.ReadLine()) (* The remaining amount of turns Thor can move. Do not remove this line. *)
+            
+        let remainingX = state.Light.X - state.Thor.X
+        let remainingY = state.Light.Y - state.Thor.Y
+
+        let nextDirection =
+            match (toSign remainingX, toSign remainingY) with
+            | ('0', '0') -> ""
+            | ('+', '0') -> "E"
+            | ('-', '0') -> "W"
+            | ('0', '+') -> "S"
+            | ('0', '-') -> "N"
+            | ('+', '+') -> "SE"
+            | ('+', '-') -> "NE"
+            | ('-', '+') -> "SW"
+            | ('-', '-') -> "NW"
+            | x -> failwithf "Unexpected value %A" x
+
+        printfn "%s" nextDirection
+        run (state.Move nextDirection)
+
+    run (State.Create lightX lightY initialTX initialTY)
