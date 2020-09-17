f5ff5696f34445861fd39de17b3f05d195c9aec8
KiritchoukC <clement.kiritchouk@gmail.com>
2020-09-17 08:16:19 +0200

    add c# and f# solution for the-descent

---
+++ the-descent/main.fsx
@@ -0,0 +1,21 @@
+﻿open System
+
+let readInputs nbInputs = List.map (fun i -> (i, Console.In.ReadLine())) nbInputs
+
+let findHighestMountain mountains =
+    List.reduce (fun acc value ->
+        match snd value > snd acc with
+        | true -> value
+        | false -> acc) mountains
+    
+let printAnswer answer = printfn "%i" (fst answer)
+
+let rec run () =
+    [ 0 .. 7 ]
+    |> readInputs
+    |> findHighestMountain
+    |> printAnswer
+
+    run ()
+
+run ()
